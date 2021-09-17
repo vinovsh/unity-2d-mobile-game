046f6d5d69f6aaebc70c025d828a00ccd7cb0b5b
madhavan1098 <madhavan1098@gmail.com>
2021-09-17 11:55:49 +0530

    first commit

--- Assets/script/main-menu/MainLoadScene.cs
+++ Assets/script/main-menu/MainLoadScene.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainLoadScene : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+    public string sceneName;
+    public GameObject Transition;
+
+    public Animator loading;
+    public string  loading_pos;
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+       if(loading_pos=="end"){
+
+           StartCoroutine(endAnim()); 
+       }
+       
     }
+public void startButton(){
+
+   StartCoroutine(LoadScene());
+}
+
+public void quitButton(){
+
+     Application.Quit();
+}
+
+IEnumerator LoadScene(){
+
+    
+
+       Transition.SetActive(true);
+       loading.SetInteger("start",1);
+       yield return new WaitForSeconds(1.5f);
+       SceneManager.LoadScene(sceneName);
+  
+
+ 
+}
+
+IEnumerator endAnim(){
+
+    
+
+       Transition.SetActive(true);
+       loading.SetInteger("end",1);
+       yield return new WaitForSeconds(1.5f);
+       Transition.SetActive(false);
+  
+
+ 
+}
+
 }
